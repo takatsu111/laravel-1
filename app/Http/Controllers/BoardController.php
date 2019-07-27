96d1899996174a1fa414efaaf73ed966c25656a3
takatsu111 <k770278@gmail.com>
2019-07-27 15:17:09 +0900

    BoardController.phpのindexアクションを、boards.show-postへのviewを返すように変更

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class BoardController extends Controller
 {
     public function index(){
-        
+        return view('boards.show-post');
     }
     
     public function post(){
