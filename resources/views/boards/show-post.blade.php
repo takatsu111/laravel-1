dcee11b5abc79c1cc3fa6bb5bf53114bad9ea7b7
takatsu111 <k770278@gmail.com>
2019-07-27 16:41:37 +0900

    show-post.blade.phpに投稿用のフォームを作成

--- resources/views/boards/show-post.blade.php
+++ resources/views/boards/show-post.blade.php
@@ -2,6 +2,44 @@
 
 
 @section('content')
-<h1>HELLOOOOOOOOO</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('投稿') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('boardPost') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('本文') }}</label>
+
+                            <div class="col-md-6">
+                                <textarea rows="10" id="post-content" class="form-control @error('post-content') is-invalid @enderror" name="post-content" value="{{ old('post-content') }}" required autofocus>
+                                </textarea>
+
+                                @error('post-content')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('投稿') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
