3c1a0286c32f2e31e1da06a6590e6a1e9339705d
takatsu111 <k770278@gmail.com>
2019-07-31 15:15:55 +0900

    categoriesテーブルseederの内容変更

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -14,7 +14,7 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
          DB::table('categories')->insert([
-            'name' => str_random(10),
+            'name' => "プログラミング",
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
