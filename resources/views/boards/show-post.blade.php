e8938a9beab233690db69f10c6b1b4d85bd9abb3
takatsu111 <k770278@gmail.com>
2019-08-01 18:46:23 +0900

    投稿フォーム位置および掲示板昇順修正

--- resources/views/boards/show-post.blade.php
+++ resources/views/boards/show-post.blade.php
@@ -17,7 +17,33 @@ errorrrrrr
     </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
-           @auth
+           
+            <div class="card p-board u-board">
+                <div class="card-header">
+                    <h1 class="c-title">{{ $board['name'] }}</h1>
+                </div>
+                @foreach ($board->contents as $content)
+                
+                <div class="c-board_content">
+                    <p class="c-paragraph u-content_user">{{$content->user['name']}}さん</p>
+                    <p class="c-paragraph u-content_text">{{$content["content"]}}</p>
+                    <div class="u-daytime_and_good">
+                        <p class="c-paragraph u-daytime">{{$content["created_at"]}}</p>
+                        @auth
+                        <form action="{{route('boardGood')}}" method="POST">
+                        @csrf
+                        <button type="submit" name="function" value=@if($content->usersOfGoods->contains($userid)) "delete" @else "insert" @endif class="btn c-paragraph u-good  @if($content->usersOfGoods->contains($userid)) btn-danger @else btn-primary u-active @endif ">いいね:{{$content->usersOfGoods->count()}}</button>
+                        <input type="hidden" name="content_id" value="{{$content->id}}">
+                        </form>
+                        @else
+                        <p class="c-paragraph u-good">いいね:{{$content->usersOfGoods->count()}}</p>
+                        @endauth
+                    </div>
+                </div>
+
+                @endforeach
+            </div>
+            @auth
             <div class="card p-board_form">
                 <div class="card-header">{{ __('投稿') }}</div>
 
@@ -52,32 +78,6 @@ errorrrrrr
                 </div>
             </div>
 @endauth
-            <div class="card p-board u-board">
-                <div class="card-header">
-                    <h1 class="c-title">{{ $board['name'] }}</h1>
-                </div>
-                @foreach ($board->contentsOrderByDesc as $content)
-                
-                <div class="c-board_content">
-                    <p class="c-paragraph u-content_user">{{$content->user['name']}}さん</p>
-                    <p class="c-paragraph u-content_text">{{$content["content"]}}</p>
-                    <div class="u-daytime_and_good">
-                        <p class="c-paragraph u-daytime">{{$content["created_at"]}}</p>
-                        @auth
-                        <form action="{{route('boardGood')}}" method="POST">
-                        @csrf
-                        <button type="submit" name="function" value=@if($content->usersOfGoods->contains($userid)) "delete" @else "insert" @endif class="btn c-paragraph u-good  @if($content->usersOfGoods->contains($userid)) btn-danger @else btn-primary u-active @endif ">いいね:{{$content->usersOfGoods->count()}}</button>
-                        <input type="hidden" name="content_id" value="{{$content->id}}">
-                        </form>
-                        @else
-                        <p class="c-paragraph u-good">いいね:{{$content->usersOfGoods->count()}}</p>
-                        @endauth
-                    </div>
-                </div>
-
-                @endforeach
-            </div>
-            
         </div>
     </div>
 </div>
