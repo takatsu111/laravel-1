1e472e3cd5a0a78ec6a2d63bf3fd786a9896571c
takatsu111 <k770278@gmail.com>
2019-08-22 18:55:16 +0900

    migration追加

---
+++ database/migrations/2019_08_05_181603_add_foreign_key_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('provider_id')->references('id')->on('providers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['provider_id']);
+        });
+    }
+}
