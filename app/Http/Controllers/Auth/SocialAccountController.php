f0ee8a0a46404f1d05e90282eb9e618efd1a8d1f
takatsu111 <k770278@gmail.com>
2019-08-22 18:31:20 +0900

    修正

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+use App\User;
 
 class SocialAccountController extends Controller
 {
@@ -15,6 +18,10 @@ class SocialAccountController extends Controller
     public function handleProviderCallback($provider)
     {
         $user = \Socialite::with($provider)->stateless()->user();
-        return redirect('/')->with('flash_message',$user->getId().'アカウントでのログインが完了しました。');
+        $login_user = User::where('social_id',$user->getId())->first();
+        if($login_user){
+            Auth::login($login_user);
+            return redirect('/')->with('flash_message',$user->getId().'アカウントでのログインが完了しました。');
+        }
     }
 }
